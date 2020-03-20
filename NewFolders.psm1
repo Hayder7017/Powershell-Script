fdd1f1f76721dccf92bcacd7a1dead4cf45e3ec6
Hayder7017 <62419583+Hayder7017@users.noreply.github.com>
2020-03-20 09:16:37 +0100

    Add files via upload

---
+++ NewFolders.psm1
@@ -0,0 +1,11 @@
+﻿Write-Host "The Powershell module is loading" -ForegroundColor Green
+Function Get-NewFolders {
+
+
+$Paths = Get-Content "C:\Powershell Scripts\NewFolders.txt"
+ForEach ($path in $Paths)
+ {
+     $newPath = Join-Path "C:\Test new folders" -ChildPath $path
+     New-Item $newPath -type Directory
+     }
+ } 
