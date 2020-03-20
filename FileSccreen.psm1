e78f38a8460c78064a4f6c00cd22a09e3bce6ef4
Hayder7017 <62419583+Hayder7017@users.noreply.github.com>
2020-03-20 09:16:04 +0100

    Add files via upload

---
+++ FileSccreen.psm1
@@ -0,0 +1,10 @@
+﻿Write-Host "The Powershell module is loading" -ForegroundColor Green
+Function Get-FSRMfileScreenhayals {
+
+
+$Paths = Get-Content "\\hayals-file01\ReadOnly\NetworkFolders.txt"
+ForEach ($path in $Paths)
+ {
+     New-FSRMfileScreen $path -Template "Block Audio and Video Files" 
+     }
+ }
